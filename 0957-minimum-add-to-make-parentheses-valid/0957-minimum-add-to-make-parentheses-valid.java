f4be03cf7651189e218442caf7f5b56aa0fc0f47
BALASUBRAMANIAN K <138891732+BHALA2004@users.noreply.github.com>
2024-08-28 11:31:50 +0530

    Time: 1 ms (64.5%), Space: 41.4 MB (45.54%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        
+    
+            while(s.contains("()")){
+                s=s.replace("()","");
+            
+        }
+        return s.length();
+    }
+}
